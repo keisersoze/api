ada88ad417cd3a2d5235c2a5af47577a231d8302
Unknown <filippomaganza@gmail.com>
2018-12-17 10:07:24 +0100

    rate limiter

--- src/main/java/com/lynx/api/filters/PreFilter.java
+++ src/main/java/com/lynx/api/filters/PreFilter.java
@@ -1,16 +1,31 @@
 package com.lynx.api.filters;
 
-import com.netflix.zuul.ZuulFilter;
-import com.netflix.zuul.context.RequestContext;
+import java.time.Duration;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+
+import io.github.bucket4j.Bandwidth;
+import io.github.bucket4j.Bucket;
+import io.github.bucket4j.Bucket4j;
+import io.github.bucket4j.Refill;
+
 @Component
 public class PreFilter extends ZuulFilter {
+	
+	private Bucket createNewBucket() {
+        long overdraft = 600; 
+        Refill refill = Refill.intervally(60, Duration.ofSeconds(1));
+        Bandwidth limit = Bandwidth.classic(overdraft, refill);
+        return Bucket4j.builder().addLimit(limit).build();
+   }
 
     private static Logger log = LoggerFactory.getLogger(PreFilter.class);
 
@@ -31,10 +46,31 @@ public class PreFilter extends ZuulFilter {
 
     @Override
     public Object run() {
-
-        RequestContext ctx = RequestContext.getCurrentContext();
+    	
+    	RequestContext ctx = RequestContext.getCurrentContext();
         HttpServletRequest request = ctx.getRequest();
+        HttpSession session = request.getSession(true);
+        
+
+        /* create a state for the application*/
+        String appKey = "123";
+        Bucket bucket = (Bucket) session.getAttribute("throttler-" + appKey);
+        if (bucket == null) {
+            bucket = createNewBucket();
+            session.setAttribute("throttler-" + appKey, bucket);
+        }
+
+        // tryConsume returns false immediately if no tokens available with the bucket
+        if (!bucket.tryConsume(1)) {
+            // blocks the request
+            ctx.setSendZuulResponse(false);
+            ctx.setResponseStatusCode(429);
+            ctx.getResponse().setHeader("Content-Type", "text/plain;charset=UTF-8");
+            ctx.setResponseBody("Too many requests");
+        }
+        
         log.info(String.format("%s request to %s", request.getMethod(), request.getRequestURL().toString()));
+        
         return null;
     }
 
